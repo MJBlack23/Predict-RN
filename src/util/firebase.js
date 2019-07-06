30b083984e9af6888103e013090b21e8f8bd324a
Matt Black <mjblack23@gmail.com>
2019-07-06 19:03:05 -0400

    removed api key

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,13 +1,14 @@
+require('dotenv').config()
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-7xUlyHZwWa4efSmFksgeuLoeKwCbEd0",
-  authDomain: "predict-a8ba8.firebaseapp.com",
-  databaseURL: "https://predict-a8ba8.firebaseio.com",
-  projectId: "predict-a8ba8",
-  storageBucket: "predict-a8ba8.appspot.com",
-  messagingSenderId: "261011000097",
-  appId: "1:261011000097:web:81780b201b2cb726"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
